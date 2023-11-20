93d840a68b7cb591f0f9828732f2a5d625059dce
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-20 18:34:44 +0000

    feature: Create Artist API implemented

--- streming-app/src/components/CreateArtist.js
+++ streming-app/src/components/CreateArtist.js
@@ -3,16 +3,25 @@ import Buttons from './Buttons';
 import Header from './Header';
 import SubHeader from './SubHeader';
 import InputText from './InputText';
+import { createAsset } from '../api/api';
 
-const CreateArtist = function CreateArtist() {
+function CreateArtist() {
     const createArtist = document.getElementsByClassName('createArtistForm')[0];
-    console.log(createArtist);
     if (createArtist && !createArtist.classList.contains('hasEventListener')) {
         createArtist.classList.add('hasEventListener');
         createArtist.addEventListener('submit', async function (e) {
             e.preventDefault();
-            const formData = new FormData(createArtist).entries()
-            console.log(formData);
+            const formData = new FormData(createArtist)
+            const values = [...formData.entries()];
+            if (values['Name'] !== '' && values['About'] !== '') {
+                const asset = {
+                    "@assetType": "artist",
+                    "name": values[0][1],
+                    "about": values[1][1]
+                }
+                createAsset(asset);
+                window.location.pathname = '/Artists'
+            }
         });
     }
     return (
