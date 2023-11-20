c5a768d398491825f6029e75f07d8d28b7bdde5b
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-20 18:33:21 +0000

    feature: Artist not found

--- streming-app/src/components/Artist.js
+++ streming-app/src/components/Artist.js
@@ -1,12 +1,12 @@
 import artistImage from '../media/Artist image.png'
 import '../style/Artist.css'
 
-function Artist(artist)
-{
-    return(
+function Artist(artist) {
+    const pathname = '/VisualizeArtist?id=' + artist['@key'];
+    return (
         <div className='artist'>
-            <img className='artistImg' src={artistImage}></img>
-            <a href='#'>{artist.name}</a>
+            <img className='artistImg' src={artistImage} alt="Artist"></img>
+            <a href={pathname}>{artist.name.length > 0 ? artist.name : "Not found"}</a>
         </div>
     )
 }
